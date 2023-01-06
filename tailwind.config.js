41d0c6b577400cf36b01a407287d99d51d63b244
ishaan1013 <69771365+ishaan1013@users.noreply.github.com>
2023-01-06 13:38:43 -0500

    finish acct popup styling

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,16 @@
 /** @type {import('tailwindcss').Config} */
+const defaultTheme = require('tailwindcss/defaultTheme')
+
 module.exports = {
   content: [
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    screens: {
+      'xs': '480px',
+      ...defaultTheme.screens,
+    },
   },
   plugins: [],
 }
